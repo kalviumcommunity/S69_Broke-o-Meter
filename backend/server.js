c624ff5136ceca0367b5c557fddf10b3b4c6650b
Vikranth2024 <vikranthsabari436@gmail.com>
2025-01-22 14:51:57 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 3100;
 
 app.get('/ping', (req,res) => {
     res.send('pong');
 });
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
